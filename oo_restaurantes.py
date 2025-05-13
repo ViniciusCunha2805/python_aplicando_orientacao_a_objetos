fe0d5ad88e333c03ef8dc22dc22fdc3fca8f7962
vinic <viniciussilvacunha2805@gmail.com>
2025-05-12 21:52:34 -0300

    Encapsulamento dos atributos da classe

--- oo_restaurantes.py
+++ oo_restaurantes.py
@@ -2,20 +2,26 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome
+        self._categoria = categoria
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
-        for restaurante in  Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}")
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+    
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza express', 'Italiana')
 restaurante_hamburguer = Restaurante('Comelao', 'Podrao')
 
-Restaurante.listar_restaurantes() 
+Restaurante.listar_restaurantes()
