25e901d6f74cffbee71b15ffff5fcc8d239f0cb5
vinic <viniciussilvacunha2805@gmail.com>
2025-05-14 17:27:50 -0300

    Criei a classe ItemCardapio, Prato, Bebida e aplique herança nelas.

---
+++ cardapio/bebida.py
@@ -0,0 +1,7 @@
+from python_aplicando_orientacao_a_objetos.cardapio.item_cardapio import ItemCardapio
+
+class Bebida(ItemCardapio):
+    def __init__(self, nome, preco, tamanho):
+        super().__init__(nome, preco)
+        self._tamanho = tamanho
+        
---
+++ cardapio/prato.py
@@ -0,0 +1,6 @@
+from python_aplicando_orientacao_a_objetos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
+    def __init__(self, nome, preco, descricao):
+        super().__init__(nome, preco)
+        self._descricao = descricao
